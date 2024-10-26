5221483e8ac2edb42f462c2b2bfc6add753dedae
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-10-26 20:23:38 -0300

    ADD: Site colors.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,7 +10,34 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-
+        background: "#F7F8FC",
+        foreground: "var(--foreground)",
+        primary: {
+          "50": "#fef1f8",
+          "100": "#fee5f3",
+          "200": "#ffcae9",
+          "300": "#ff9fd5",
+          "400": "#ff63b8",
+          "500": "#ff3c9d",
+          "600": "#f01276",
+          "700": "#d1055b",
+          "800": "#ad074b",
+          "900": "#8f0c41",
+          "950": "#580022",
+        },
+        secondary: {
+          "50": "#eff1fe",
+          "100": "#e1e6fe",
+          "200": "#cad1fb",
+          "300": "#a9b2f8",
+          "400": "#8789f2",
+          "500": "#706aea",
+          "600": "#6a5ae0",
+          "700": "#513fc3",
+          "800": "#43359e",
+          "900": "#3a327d",
+          "950": "#231d49",
+        },
       },
     },
   },
