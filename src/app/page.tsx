b941532f4b54b6f309fe2e23b79f59df4d724c4f
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-10-31 16:56:02 -0300

    FEAT: Add area filters on home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,28 @@
-import TechnologyCard from "@/components/cards/TechnologyCard";
+import TechnologiesGrid from "@/components/home/TechnologiesGrid";
 import prisma  from "@/lib/prisma";
 
 export default async function Home() {
-  const quizzes = await prisma.technology.findMany({
+  const areas = await prisma.area.findMany({
+    where: {
+      technologies: {
+        some: {
+          quiz: {
+            some: {}
+          }
+        }
+      }
+    },
     include: {
-      area: true,
-      quiz: true
+      technologies: {
+        include: {
+          quiz: true
+        }
+      }
     }
   });
+  
 
   return (
-    
-    <div className="grid grid-cols-2 sm:grid-cols-4 gap-2 sm:gap-4">
-      {
-        quizzes.map((quiz) => (
-          <TechnologyCard key={quiz.slug} data={{
-            title: quiz.name,
-            area: quiz.area.name,
-            slug: quiz.slug,
-            icon: quiz.icon,
-            quizzes: quiz.quiz.length
-          }} />
-        ))
-      }
-    </div>
+    <TechnologiesGrid areas={areas} />
   );
 }
---
+++ src/components/home/TechnologiesGrid.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import TechnologyCard from "@/components/cards/TechnologyCard";
+import { Area, Technology, Quiz } from "@prisma/client";
+import { useState, useEffect } from "react";
+
+interface TechnologyGridProps {
+  areas: (Area & {
+    technologies: (Technology & {
+      quiz: Quiz[];
+    })[];
+  })[];
+}
+
+export default function TechnologiesGrid({ areas }: TechnologyGridProps) {
+
+  const [currentArea, setCurrentArea] = useState("todos");
+  const [filteredAreas, setFilteredAreas] = useState(areas);
+
+  useEffect(() => {
+    if (currentArea === "todos") {
+      setFilteredAreas(areas);
+    } else {
+      const filtered = areas.filter((area) => area.name === currentArea);
+      setFilteredAreas(filtered);
+    }
+  }, [currentArea, areas]);
+
+  return (
+    <div>
+      {/* Filtros */}
+      <ul className="flex gap-3 mb-4">
+        <li className="text-sm">
+          <button 
+            className={`app-card py-2 px-4 ${currentArea === "todos" && "!bg-primary-500 !text-white font-bold"}`} 
+            onClick={() => setCurrentArea("todos")}>
+              Todos
+          </button>
+        </li>
+        {
+          areas.map((area) => (
+            <li key={area.id} className=" text-sm">
+              <button 
+                className={`app-card py-2 px-4 ${currentArea === area.name && "!bg-primary-500 !text-white font-bold"}`}
+                onClick={() => setCurrentArea(area.name)}>
+                  {area.name}
+              </button>
+            </li>
+          ))
+        }
+      </ul>
+
+       {/* Grid de tecnologias */}
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-2 sm:gap-4">
+        {filteredAreas.map((area) =>
+          area.technologies.map((technology) => (
+            <TechnologyCard
+              key={technology.slug}
+              data={{
+                title: technology.name,
+                area: area.name,
+                slug: technology.slug,
+                icon: technology.icon,
+                quizzes: technology.quiz.length,
+              }}
+            />
+          ))
+        )}
+      </div>
+    </div>
+  );
+}
