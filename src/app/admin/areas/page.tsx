172475c5a41ef3fd7bada60fdac2810960e664c3
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-11-02 00:06:31 -0300

    UPD: Area table.

--- src/app/admin/areas/page.tsx
+++ src/app/admin/areas/page.tsx
@@ -1,4 +1,5 @@
 import SectionsHeader from "@/components/admin/AdminSectionHeader";
+import AppButton from "@/components/AppButton";
 import prisma from "@/lib/prisma";
 
 export default async function CategoriesPage() {
@@ -29,23 +30,43 @@ export default async function CategoriesPage() {
         buttonText="Añadir Area"
       />
       <div className="app-tabla">
-        <div className="app-tabla__header grid grid-cols-3 p-4">
+        {/* Tabla Header */}
+        <div className="app-tabla__header hidden md:grid grid-cols-[1.2fr_1fr_1fr_120px] p-4">
           <p className="font-semibold">Nombre</p>
           <p className="font-semibold">Tecnologías</p>
           <p className="font-semibold">Quizzes</p>
+          <p className="fott-semibold">Acciones</p>
         </div>
+        {/* Tabla Body */}
         <div className="app-tabla__body mt-2">
           {areas.map((area) => (
             <div 
               className="
-                app-tabla__row grid grid-cols-3 
+                app-tabla__row grid grid-cols-1 md:grid-cols-[1.2fr_1fr_1fr_120px] items-center
                 bg-white 
                 dark:bg-blue-gray
                 shadow-sm rounded-md p-4 gap-2 mt-3" 
               key={area.id}>
-                <p>{area.name}</p>
-                <p>{area.technologies.length}</p>
-                <p>{area.technologies.reduce((acc, tech) => acc + tech.quiz.length, 0)}</p>
+                <p> 
+                  <span className="inline-block md:hidden font-bold me-2">Nombre:</span>  
+                  {area.name}
+                </p>
+                <p>
+                  <span className="inline-block md:hidden font-bold me-2">Tecnologías:</span>
+                  {area.technologies.length}
+                </p>
+                <p>
+                  <span className="inline-block md:hidden font-bold me-2">Quizzes:</span>
+                  {area.technologies.reduce((acc, tech) => acc + tech.quiz.length, 0)}
+                </p>
+                <div className="flex gap-3 justify-end md:justify-start">
+                  <AppButton
+                    text="Editar"
+                    className="text-xs !bg-blue-500 hover:bg-blue-700"
+                    buttonType="link"
+                    url={`/admin/areas/editar/${area.slug}`}
+                  />
+                </div>
             </div>
           ))}
         </div>
