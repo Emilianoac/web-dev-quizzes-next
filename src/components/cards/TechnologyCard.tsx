47ad186603fa1ecd54f36bf73c9ea09864f5c7ac
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-11-04 12:35:43 -0300

    STYLE: Add line clamping to area text in TechnologyCard component

--- src/components/cards/TechnologyCard.tsx
+++ src/components/cards/TechnologyCard.tsx
@@ -37,7 +37,7 @@ export default function QuizCard({ data, admin }: CardProps) {
               />
           </div>
           <h2 className="font-bold md:text-[1.1em] text-base">{data.title}</h2>
-          <p className="text-center text-slate-700 dark:text-slate-300 text-sm font-semibold mt-2">{data.area}</p>
+          <p className="text-center text-slate-700 dark:text-slate-300 line-clamp-1 text-sm font-semibold mt-2">{data.area}</p>
           <p className="text-center text-slate-600 dark:text-slate-400 text-sm mt-3">
             {data.quizzes} 
             {data.quizzes === 1 ? ' Quiz' : ' Quizzes'}
